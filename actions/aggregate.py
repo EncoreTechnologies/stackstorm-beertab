9996b1302b3765630ad07456d5bcea6cdd288841
Nick Maludy <nmaludy@gmail.com>
2019-04-17 14:07:46 -0400

    Remove the beertab. string in the aggregate action

--- actions/aggregate.py
+++ actions/aggregate.py
@@ -10,10 +10,11 @@ class Aggregate(Action):
         result = {}
         # find all of the tabs with recipient in them and gather them up into
         # one dict keyed on the owner who owes the beer
-        for owner, tab in six.iteritems(beertabs):
+        for kv_key, tab in six.iteritems(beertabs):
             if isinstance(tab, six.string_types):
                 tab = json.loads(tab)
 
+            owner = kv_key.replace('beertab.', 1)
             if recipient in tab:
                 result[owner] = tab[recipient]
 
